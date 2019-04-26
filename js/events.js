5064dda78d524cc5d4423b48d5a0c4bb2c17cab6
Sean Altemose <sean@seanaltemose.com>
2019-04-26 18:37:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
+function getIt(){
+  $('p').on('click', function(event)){
+    alert('Hey!');
+  });
+};
+
+function frameIt(){
+  $('img').on('load', function(event){
+    $('img').addClass("tasty")
+  })
+};
+
+function pressIt(){
+  $('input').on("keydown", function(e){
+    if e.which == 71{
+      alert('You pressed G!')
+    }
+  })
+};
+
+function submitIt(){
+  $('form').on('submit', function(e){
+    alert('Your form is going to be submitted now.')
+  })
+};
+
 $(document).ready(function(){
 
 // call functions here
+  getIt();
+  pressIt();
+  submitIt();
+  frameIt();
 
 });
