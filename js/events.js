7d8ea7f858d5c2aae033b9a76b85269daf512c52
Sean Altemose <sean@seanaltemose.com>
2019-04-26 18:38:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
-$(document).ready(function(){
 
-// call functions here
+function getIt() {
+  $('p').on('click', function(event) {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  image = $('img')
+  image.on('load', function(event) {
+    image.addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form input[type=text]').on('keydown', function(event) {
+    if(event.which == 71) {
+      alert("You've pressed letter G")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(event) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
+$(document).ready(function(){
+  getIt() ;
+  pressIt() ;
+  submitIt() ;
+  frameIt() ;
 
 });
